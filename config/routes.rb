9143c7c018aec2532da4760509d277e8f7a490df
komayuka <komayuka115@gmail.com>
2021-06-08 01:09:21 +0000

    [update]

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
   root 'homes#top'
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registations => 'users/registations',
+  }
 
   devise_for :admins
+  resources  :users, only: [:show]
+  resources :animes
 
 
 end
